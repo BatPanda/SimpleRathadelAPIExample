d8c52a6f9d96a1ba0097d46dc604401f2563c217
BatPanda <Agentekul@googlemail.com>
2021-10-03 22:54:22 +0100

    Boilerplate
    A simple boilerplate I quickly setup for others to use as an example for tool development.

---
+++ api_use_test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args) {
+        const string url = "http://multiversewiki.com:8080";
+        const string url_parameters = "/techniques";
+        HttpClient client = new HttpClient();
+        client.BaseAddress = new Uri(url);
+        client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/json"));
+            
+
+        // List data response.
+        HttpResponseMessage response = client.GetAsync(url_parameters).Result;
+        if (response.IsSuccessStatusCode) {
+            var dataObjects = response.Content;
+            TechniqueStructure techniques_data = JsonConvert.DeserializeObject<TechniqueStructure>(dataObjects.ReadAsStringAsync().Result);
+            int total_global_points = techniques_data.techniques.Aggregate(0, (_a, _b) => _a + _b.total_points);
+            int total_learning_days = (int)Math.Ceiling((float)total_global_points / 2);
+            float total_years = total_learning_days / 448;
+            Console.WriteLine($"There are currently {techniques_data.technique_quantity} techniques!");
+            Console.WriteLine($"To learn every technique you would need to allocate {total_global_points} points.");
+            Console.WriteLine($"Assuming your comprehension bonus is 1 and it cannot increase, and that you always sleep at night, it would take {Math.Ceiling((float)total_global_points / 2)} days to learn everything.");
+            Console.WriteLine($"In Rathadel that is {total_years} years!");
+                
+        } else {
+            Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+        }
+        // Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+        client.Dispose();
+    }
+}
---
+++ api_use_test/TechniqueStructure.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public enum MasteryTypes
+{
+    SMALL_SUCCESS,
+    LARGE_SUCCESS,
+    SMALL_PERFECTION,
+    GREAT_PERFECTION,
+    OBSCURE,
+    DIVINE
+
+}
+
+public struct MasteryStruct
+{
+    public MasteryTypes type; //int order is standard (0 = small success, 5 = divine) when moving from json to enum.
+    public int total_points; //total summed points from all stages within this mastery.
+    public int total_levels; //total summed levels from all stages within this mastery.
+    public int total_stages; //total number of stages in the technique.
+    public int total_levels_per_stage; //the number of levels per stage.
+    public List<StageStruct> stages; //list of stages in this mastery.
+
+    public MasteryStruct(MasteryTypes _type, int _total_points, int _total_levels, int _total_stages, int _total_levels_per_stage, List<StageStruct> _stages) {
+        type = _type;
+        total_points = _total_points;
+        total_levels = _total_levels;
+        total_stages = _total_stages;
+        total_levels_per_stage = _total_levels_per_stage;
+        stages = _stages;
+    }
+}
+
+public struct StageStruct
+{
+    public int total_points; //total summed points for this stage.
+    public int total_levels; //total summed levels for this stage.
+    public List<LevelStruct> levels; //list of levels in this stage
+
+    public StageStruct(int _total_points, int _total_levels, List<LevelStruct> _levels) {
+        total_points = _total_points;
+        total_levels = _total_levels;
+        levels = _levels;
+    }
+}
+
+public struct LevelStruct
+{
+    public int required_points; //number of points to complete this specific level.
+    public string point_type; //the type of points needed to make progress in this level. By default it is "standard". Can also be "falconry".
+    public string level_type; //the type of level this is. Can be either: "action", "auxiliary", or "increase".
+    public string level_content; //the string content of the level.
+
+    public LevelStruct(int _required_points, string _point_type, string _level_type, string _level_content) {
+        required_points = _required_points;
+        point_type = _point_type;
+        level_type = _level_type;
+        level_content = _level_content;
+    }
+}
+
+public class TechniqueData
+{
+    public int id; //the uuid of the technique.
+    public string name; //the name of the technique.
+    public string cluster; //the cluster name this technique belongs in.
+    public string grade_colour; //the string name of the grade e.g. "red"
+    public int grade_number; //the level within the grade e.g. 1.
+    public int total_points; //the total summed points from all masteries.
+    public int total_levels; //the total summed levels from all masteries.
+    public int full_stage_number; //the expected number of stages when complete.
+    public List<string> requirements; //list of requirements to use the technique.
+    public List<string> manual_location; //list of locations where the technique can be found in game.
+    public bool finished; //is the technique complete?
+    public List<MasteryStruct> masteries; //list of masteries within the technique.
+}
+
+class TechniqueStructure
+{
+    public int technique_quantity; //number of techniques.
+    public List<TechniqueData> techniques; //list of all techniques.
+}
